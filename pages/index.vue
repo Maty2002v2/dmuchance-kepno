373dd8f87d251d3a7e90fdcec36f0f39cfd494c1
Maty2002v2 <matikster21@gmail.com>
2024-03-13 18:47:29 +0100

    Translate back to top label

--- pages/index.vue
+++ pages/index.vue
@@ -549,7 +549,7 @@ const accordions = [
         href="#navbar"
         class="px-6 py-3 flex items-center space-x-2 bg-[#FAFAFA] hover:bg-gray-100 hover:shadow-md border border-[#DDDDDD] rounded-md text-gray-700"
       >
-        <span>Back to top</span>
+        <span>Powrót do góry</span>
         <ArrowUpIcon :size="20" />
       </a>
     </div>
